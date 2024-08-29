52a2bf37595daeea72f6b64eb4f8302f4064f912
devreese <anthony.devreese@student.vinci.be>
2024-08-29 17:23:32 +0200

    add: a year divisible by 4 AND not by 100 except by 400 is a leapYear

--- __test__/anneeBissextile.test.js
+++ __test__/anneeBissextile.test.js
@@ -1,12 +1,18 @@
 const isAnneeBissextile = require('../anneeBissextile');
 
-test('Les année divisibles par 400 sont bissextiles', () => {
+test('Les années divisibles par 400 sont bissextiles', () => {
     expect(isAnneeBissextile(2000)).toBeTruthy();
 })
 
-test('Les année divisibles par 100 mais pas 400 ne sont pas bissextiles', () => {
+test('Les années divisibles par 100 mais pas 400 ne sont pas bissextiles', () => {
     expect(isAnneeBissextile(1700)).toBe(false);
     expect(isAnneeBissextile(1800)).toBe(false);
     expect(isAnneeBissextile(1900)).toBe(false);
     expect(isAnneeBissextile(2100)).toBe(false);
+})
+
+test('toutes les années divisibles par 4 mais pas par 100 sont des années bissectiles', () => {
+    expect(isAnneeBissextile(2008)).toBeTruthy();
+    expect(isAnneeBissextile(2012)).toBeTruthy();
+    expect(isAnneeBissextile(2016)).toBeTruthy();
 })
--- anneeBissextile.js
+++ anneeBissextile.js
@@ -1,5 +1,12 @@
 function isAnneeBissextile(annee) {
-    return annee % 400 === 0;
+    if (annee % 400 === 0) {
+        return true;
+    } else if (annee % 100 === 0) {
+        return false;
+    } else if (annee % 4 === 0) {
+        return true;
+    }
+    return false;
 }
 
 module.exports = isAnneeBissextile;

b099d50c3d95bdae196a5dbda620824a1c7fe3e9
devreese <anthony.devreese@student.vinci.be>
2024-08-29 17:17:13 +0200

    add: a year is not a leapYear if it is divisible by 100 but not 400

--- __test__/anneeBissextile.test.js
+++ __test__/anneeBissextile.test.js
@@ -3,3 +3,10 @@ const isAnneeBissextile = require('../anneeBissextile');
 test('Les année divisibles par 400 sont bissextiles', () => {
     expect(isAnneeBissextile(2000)).toBeTruthy();
 })
+
+test('Les année divisibles par 100 mais pas 400 ne sont pas bissextiles', () => {
+    expect(isAnneeBissextile(1700)).toBe(false);
+    expect(isAnneeBissextile(1800)).toBe(false);
+    expect(isAnneeBissextile(1900)).toBe(false);
+    expect(isAnneeBissextile(2100)).toBe(false);
+})
